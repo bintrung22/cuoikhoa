94ca98442da550fa9db0ab833c1dd6f813df7744
MSI\trung <bin.trung0@gmail.com>
2023-03-31 21:19:58 +0700

    xong event doi check code roi tiep tuc chinh sua

--- models/eventModel.js
+++ models/eventModel.js
@@ -5,7 +5,7 @@ const eventSchema = mongoose.Schema({
   eventTitle:{type:String, require:true},
   eventItems:[{type:String, require:true}],
   eventDescription:{type:String, require:true},
-  createdAt:{type:Date, require:true},
+  createdAt:{type:Date, default:Date.now()},
   imageList:[{type:String, require:true}],
   organizer:{type: mongoose.Schema.Types.ObjectId, required: true, ref: "User"},
   location:{type:String, require:true}
